bad53f3fe35978bf30e3aedb4348d87bab8f95d0
mhkeller <michael.keller@gmail.com>
2015-07-20 11:20:14 -0400

    add menu files and init stage

---
+++ init.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var cheerio = require('cheerio')
+
+// Set paths
+var index_path = './node_modules/chartbuilder/build/index.html',
+		menu_path = './menus.html';
+
+// Read our menu html and our index html
+var menus = fs.readFileSync(menu_path, 'utf-8'),
+    index = fs.readFileSync(index_path, 'utf-8')
+
+// Read `index.html` in
+var $ = cheerio.load(index)
+
+// Add menu scripts
+$('body').append(menus);
+
+// Rewrite `index.html`
+fs.writeFileSync(index_path, $.html());
