913d93f051ebc54e2048493398978018cd2d1aa7
mhkeller <michael.keller@gmail.com>
2015-07-20 12:03:05 -0400

    actually make menu items idempotent

--- init.js
+++ init.js
@@ -1,21 +1,25 @@
 #!/usr/bin/env node
 
-var fs = require('fs');
+var fs = require('fs')
 var cheerio = require('cheerio')
 
 // Set paths
 var index_path = './node_modules/chartbuilder/build/index.html',
-		menu_path = './menus.html';
+		menu_path = './menus.html'
 
 // Read our menu html and our index html
 var menus = fs.readFileSync(menu_path, 'utf-8'),
-    index = fs.readFileSync(index_path, 'utf-8')
+    index = fs.readFileSync(index_path, 'utf-8');
 
 // Read `index.html` in
 var $ = cheerio.load(index)
 
 // Add menu scripts
-$('body').append(menus);
+var $body = $('body'),
+    has_menus = $body.find('script#electron-menus').length > 0;
 
-// Rewrite `index.html`
-fs.writeFileSync(index_path, $.html());
+if (!has_menus) {
+	$body.append(menus)
+	// Rewrite `index.html`
+	fs.writeFileSync(index_path, $.html())
+}
